f7b02c50fcc9248f164338f89774de57b6d6800d
Vongola <me@vongola.tw>
2024-06-10 00:42:40 +0800

    Add api doc artifact

---
+++ utils/export_openapi.py
@@ -0,0 +1,19 @@
+# pylint: disable=import-outside-toplevel
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent.joinpath("src")))
+
+from fastapi.openapi.utils import get_openapi
+import json
+from main import app
+
+
+with open('docs/openapi.json', 'w+') as fd:
+    json.dump(get_openapi(
+        title=app.title,
+        version=app.version,
+        openapi_version=app.openapi_version,
+        description=app.description,
+        routes=app.routes,
+    ), fd)
